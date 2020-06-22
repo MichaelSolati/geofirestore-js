bb31990720da18164f3431369c1f7b96cf315e7c
Michael Solati <mkslt04@gmail.com>
2020-06-21 21:27:35 -0700

    feat(initializeApp): add function to create new GeoFirestore instance

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 export {GeoFirestoreTypes} from 'geofirestore-core';
-
-export * from './GeoCollectionReference';
-export * from './GeoDocumentReference';
-export * from './GeoDocumentSnapshot';
-export * from './GeoFirestore';
-export * from './GeoQuery';
-export * from './GeoTransaction';
-export * from './GeoWriteBatch';
+export {GeoCollectionReference} from './GeoCollectionReference';
+export {GeoDocumentReference} from './GeoDocumentReference';
+export {GeoDocumentSnapshot} from './GeoDocumentSnapshot';
+export {GeoFirestore} from './GeoFirestore';
+export {GeoQuery} from './GeoQuery';
+export {GeoTransaction} from './GeoTransaction';
+export {GeoWriteBatch} from './GeoWriteBatch';
+export {initializeApp} from './utils';
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,11 +1,18 @@
 import * as chai from 'chai';
 
-import {sanitizeSetOptions} from '../src/utils';
-import {dummySetOptions} from './common';
+import {GeoFirestore} from '../src/GeoFirestore';
+import {initializeApp, sanitizeSetOptions} from '../src/utils';
+import {dummySetOptions, firestore} from './common';
 
 const expect = chai.expect;
 
 describe('Utils Tests:', () => {
+  describe('Initialize App:', () => {
+    it('initializeApp() returns new instance of GeoFirestore', () => {
+      expect(initializeApp(firestore)).to.be.instanceOf(GeoFirestore)
+    });
+  });
+
   describe('Sanitize SetOptions:', () => {
     it('sanitizeSetOptions() removes firestore-invalid keys from SetOptions', () => {
       const {merge, mergeFields} = dummySetOptions;
